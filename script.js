d9e53f0f18f6c10962ba6be2950d45bc758d8c95
Odezulu Chukwuemeka <odezuluaugustine@gmail.com>
2022-07-28 12:05:26 +0100

    added comment

--- script.js
+++ script.js
@@ -2,37 +2,22 @@
 function checkValue(){
   let number = 200;
   for (let i = 1; i <= number; i++) {
+    //for numbers that are divisible by 6 and 8
     if ((i % 8 == 0 && i % 6 == 0)){
       console.log("ShamsTIIDELAB");
     }
+    //for numbers divisible by 6 only
     else if (i % 6 == 0) {
       console.log('Shams');
     }
+    //for numbers divisible by 8 only
     else if (i % 8 == 0){
       console.log('TIIDELAB');
     }
+    //for numbers that are neither divisible by 6 nor 8
     else
     console.log(i);
   }
 }
 checkValue();
 
-//Assignment two
-function createChessBoard(number) {
-  //declaration
-  // let number = 8;
-  let chessBoard = '';
-  for (let index = 0; index < number; index++) {
-    for (let x = 0; x < number; x++) {
-      if ((x + index) % 2 == 0){
-        chessBoard += " ";
-      }
-      else{
-        chessBoard += "#";
-      }
-    }
-    chessBoard += `\n`;
-  }
-  console.log(chessBoard);
-}
-createChessBoard();
